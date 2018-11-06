d1dac465cbbaa55c74226c8a8c3d05262a66354b
Ariel <ariel@zplinux.com>
2018-11-06 14:08:01 +0100

    Add and use 'concurrently' package + set localhost as default server adress

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 // to import in client side
-exports.API_SERVER_HOST = '192.168.0.8';
+exports.API_SERVER_HOST = '127.0.0.1';
 exports.API_SERVER_PORT = '8000';
 
 // to import in server side
-exports.API_SERVER_HOST_INTERNAL = '192.168.0.8';
+exports.API_SERVER_HOST_INTERNAL = '127.0.0.1';
 exports.API_SERVER_PORT_INTERNAL = '8000';
